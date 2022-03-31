f5a94dd4bef8a51e59b077f713592eab04b92382
MATIAS EZEQUIEL PASTELOFF <46366155@ort>
2022-03-31 10:37:57 -0300

    Mensaje

---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+const arr = [1, 2, 4, 12, 43, 55, 66, 0, -1];
+var largest= 0;
+
+const odds = arr.filter(number => {
+  return number % 2 !== 0;
+});
+
+for (i=0; i<=largest;i++){
+    if (odds[i]>largest) {
+        var largest=odds[i];
+    }
+}
+console.log()
+console.log("The odd numbers are: " + odds)
+console.log("The highest odd number is: " + largest);
+console.log()

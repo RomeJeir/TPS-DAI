898ce9f3bf43e69d4abd0fe42fbd7f7e460070ab
ROMEO JEIRFIMCZUK ALMEIDA <46583925@ort>
2022-04-18 12:04:25 -0300

    Mensaje

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,25 +1,40 @@
-//a
-
-let palosNegros = 2;
-let numerosPares = 5;
-
-let multiplicarNegrosYPares = palosNegros * numerosPares;
-
-console.log("Las cartas negras y pares son: " + multiplicarNegrosYPares);
-
-//b
-let palos = ["Picas","Diamantes","Corazones","Treboles"]
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-let carta = {
-    palo: "",
-    numero: 0,
-}
-for(i = 0; i < numeros.length; i++){
-    for(j = 0; j < palos.length; j++){
-        console.log(numeros [i], palos [j])
+//a y b
+const palos = ["Picas","Diamantes","Corazones","Treboles"]
+let figuras = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+const mazo = [];
+for(const palo of palos){
+    const color = palo=== 'diamante' || palo ==='corazon' ? 'rojo' : 'negro'
+    for(const figura of figuras){
+        mazo.push({
+            palo,
+            figura,
+            color,
+        });
     }
 }
 
-function getRandomArbitrary(numeros) {
-    numeros.splice(Math.random() * (14 - 1) + 1)
+console.log(mazo)
+
+let count = 0;
+
+for(const carta of mazo){
+    if(carta.color === "negro" && carta.figuras % 2 === 0){
+        count ++;
+    }
 }
+
+console.log('Existen ' + count + ' cartas par y negras');
+
+const carta_al_azar = (mazo) => Math.round(Math.random() * mazo.length)
+
+const rndl = carta_al_azar(mazo);
+
+console.log(rndl);
+
+const eliminadas = []
+
+for(let i = 1; i < 7; i++){
+    eliminadas.push (carta_al_azar(mazo));
+}
+
+console.log(eliminadas)

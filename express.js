b7789cbf9555be7b2d9a853a3e00c3917bcc3253
MATIAS EZEQUIEL PASTELOFF <46366155@ort>
2022-04-28 07:43:08 -0300

    Mensaje

--- express.js
+++ express.js
@@ -15,44 +15,56 @@ app.listen(PORT,(err) => {
 	console.log("Server listening on PORT", PORT);
 })
 
-app.post("/numero_aleatorio", (req,res) => {
+
+let num_max = prompt("Ingrese la cantidad maximo de numeros:", "");
+let cant_cartones = numero_aleatorio(num_max);
+let bingo_max = 25;
+
+
+function obtener_carton(nom_jug){
+    let cart = iniciar_juego();
+    for(g = 0;g<cant_cartones;g++)
+    {
+        for(i=0;i<bingo_max;i++)
+        {
+            let nums_bing = Math.Random(1,100);
+        }
+        cart[g].nums_bingo.push(nums_bing)
+    }
+}
+
+function iniciar_juego(){
+    let cartones = [];
+    for(i = 0;i<cant_cartones;i++){
+        let carton = {
+            id: "id" + i,
+            nums_bingo: []
+        }
+        cartones.push(carton);
+    }
+    return cartones;
+}
 
 function numero_aleatorio(num_max)
 {
     let num_ran = Math.random(1,num_max);
     return num_ran;
 }
+
+
+app.post("/numero_aleatorio", (req,res) => {
+    rando_num = numero_aleatorio(num_max)
+    res.send(rando_num)
 })
 
-let num_max = prompt("Ingrese la cantidad maximo de numeros:", "");
-let cant_cartones = numero_aleatorio(num_max);
-let bingo_max = 25;
-
 app.post("/iniciar_juego", (req,res) => {
-    function iniciar_juego(){
-        let cartones = [];
-        for(i = 0;i<cant_cartones;i++){
-            let carton = {
-                id: "id" + i,
-                nums_bingo: []
-            }
-            cartones.push(carton);
-        }
-        return cartones;
-    }
+    start = iniciar_juego()
+    res.send(start)
 })
 
 app.get("/obtener_carton", (req,res) => {
     let nom_jug = prompt("Ingrese el nombre del jugador:", "");
-    function obtener_carton(nom_jug){
-        let cart = iniciar_juego();
-        for(g = 0;g<cant_cartones;g++)
-        {
-            for(i=0;i<bingo_max;i++)
-            {
-                let nums_bing = Math.Random(1,100);
-            }
-            cart[g].nums_bingo.push(nums_bing)
-        }
-    }
+    obt_cart = obtener_carton(nom_jug)
+    res.send(obt_cart)
 })
+
